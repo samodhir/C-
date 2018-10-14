b84829c893122193de84f57e25ff739c34fde61c
samodhir <samodhiruwangi@gmail.com>
2018-10-14 12:51:25 +0530

    First Example Hello World

---
+++ MyProject1/Examples/Example1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject1.Examples
+{
+    class Example1
+    {
+        static void Main()
+        {
+            Console.Write("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
