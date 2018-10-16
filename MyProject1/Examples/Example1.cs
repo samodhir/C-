b2280644c81c31424c2938985107eef2a1f8e70c
samodhir <samodhiruwangi@gmail.com>
2018-10-16 06:01:00 +0530

    Add Two Integers

--- MyProject1/Examples/Example1.cs
+++ MyProject1/Examples/Example1.cs
@@ -10,9 +10,16 @@ namespace MyProject1.Examples
     {
         static void Main()
         {
-            Console.Write("Enter your Name ");
-            String name = Console.ReadLine();
-            Console.Write("Hello " + name);
+            Console.Write("Enter your First Number ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your Second Number ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            Console.Write("You Entered {0} and {1} Sum of num1 and num2 = {2} ", num1,num2,sum);
+            Console.Write("", sum);
             Console.ReadKey();
         }
     }
